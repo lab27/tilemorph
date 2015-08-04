6c0e7d1727264b579e1ab54039340d19cb5fcb3e
Nick Niles <nickyniles@gmail.com>
2015-08-04 15:09:12 +0200

    finished buttons

--- app/js/app.js
+++ app/js/app.js
@@ -24,7 +24,48 @@ var app = (function(document, $) {
 })();
 
 
-$('#padding1').on('click',function(){
-	console.log('test')
-	$('.thumbnail-inside').addClass('no-padding')
+
+$('#image1').on('click',function(){
+	$('.thumb-image-holder').toggleClass('no-margin');
+	$(this).toggleClass('success')
 });
+
+
+$('#image-color').on('click',function(){
+	$('.thumb-image').toggleClass('filters');
+	$(this).toggleClass('success')
+});
+
+
+$('#tile-width').on('click',function(){
+	$('.thumbnail').toggleClass('old-width');
+	$(this).toggleClass('success')
+});
+
+
+$('#oldpub').on('click',function(){
+	$('.old-publisher').toggleClass('hide-for-small-up');
+	$('.old-date').toggleClass('hide-for-small-up');
+	$('.thumb-meta').toggleClass('hide-for-small-up');
+	$(this).toggleClass('success')
+});
+
+
+$('#tilebg').on('click',function(){
+	$('.thumbnail-inside').toggleClass('oldbackground');
+	$(this).toggleClass('success')
+});
+
+
+$('#title').on('click',function(){
+	$('.thumb-title').toggleClass('old-style');
+	$('.thumb-logline').toggleClass('old-style');
+	$(this).toggleClass('success')
+});
+
+
+$('#actions').on('click',function(){
+	$('.thumb-actions').toggleClass('hide-for-small-up');
+	$(this).toggleClass('success')
+});
+
